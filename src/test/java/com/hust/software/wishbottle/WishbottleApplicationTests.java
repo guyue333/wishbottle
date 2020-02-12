5a65bae12da87805a24b67287759a3f8f4ded874
tom_glb <goodMorning_glb@atguigu.com>
2020-02-12 18:04:58 +0800

    Initial commit

---
+++ src/test/java/com/hust/software/wishbottle/WishbottleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hust.software.wishbottle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WishbottleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
